093484240fde541f0e38c2a81c96903e1b0699f1
frank <franklin.neves.filho@gmail.com>
2024-10-17 13:32:15 -0400

    Updated resume page

--- src/screens/ResumeScreen.tsx
+++ src/screens/ResumeScreen.tsx
@@ -2,7 +2,7 @@
 export function ResumeScreen() {
     return (
         <iframe
-            src="/resume.pdf"
+            src="/resume-10-1-24.pdf"
             style={{ width: "100vw", height: "100vh" }}
             title="Resume PDF"
         />
